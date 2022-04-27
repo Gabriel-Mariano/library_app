03ee5baf246f153d8d794d93e7e297003b0d14cd
Gabriel Mariano <gabrielmarianoo93@gmail.com>
2022-04-27 15:27:15 -0300

    feat: Criado a tela main

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Hi Devs 📚</Text>
+        </View>
+    )
+}
+
+export default Main;
